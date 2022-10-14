b3d78c5bf0cf0cdb8f41bac8bf8f0d5c9afd91cf
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-10-14 20:42:41 +0800

    Updated tracker layout

--- src/layouts/TrackerLayout.vue
+++ src/layouts/TrackerLayout.vue
@@ -2,33 +2,50 @@
   <q-layout view="lHh Lpr lFf">
     <q-header class="bg-deep-purple-13" elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-space />
-
-        <div class="q-gutter-md">
-          <q-btn flat color="white" round icon="notifications">
-            <q-badge color="red" floating rounded="">22</q-badge>
-          </q-btn>
-          <q-avatar size="40px">
-            <img :src="`https://cdn.quasar.dev/img/avatar1.jpg`" />
-          </q-avatar>
+        <div class="col-1 col-sm-1">
+          <q-btn
+            v-if="backButtonVisibility"
+            icon="west"
+            aria-label="Back"
+            round
+            dense
+            flat
+            @click="onBack"
+          />
         </div>
+
+        <div class="col col-sm text-center">
+          <span class="text-h6">{{ getTitle }}</span>
+        </div>
+
+        <div class="col-1 col-sm-1"></div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header> Navigation </q-item-label>
-      </q-list>
-    </q-drawer>
+    <q-footer bordered class="bg-deep-purple-13">
+      <q-tabs
+        no-caps
+        active-color="white"
+        indicator-color="transparent"
+        class="text-grey"
+        v-model="selectedTab"
+        dense
+      >
+        <q-tab name="home" icon="home" label="Home" />
+        <q-tab name="income" icon="account_balance_wallet" label="Income" />
+        <q-tab name="expenses" icon="shopping_cart" label="Expenses" />
+        <q-tab name="savings" icon="savings" label="Savings" />
+        <q-tab
+          v-if="hasUnreadMessag"
+          name="profile"
+          icon="account_circle"
+          label="Profile"
+          alert="red"
+          alert-icon="mark_unread_chat_alt"
+        />
+        <q-tab v-else name="profile" icon="account_circle" label="Profile" />
+      </q-tabs>
+    </q-footer>
 
     <q-page-container>
       <router-view />
@@ -37,18 +54,36 @@
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, computed } from "vue";
+import { useRouter } from "vue-router";
 
 export default defineComponent({
-  name: "MainLayout",
+  name: "TrackerLayout",
 
   setup() {
-    const leftDrawerOpen = ref(false);
+    const selectedTab = ref("home");
+    const router = useRouter();
+
+    const backButtonVisibility = computed(() => {
+      return router.currentRoute.value.meta.withBackButton;
+    });
+
+    const getTitle = computed(() => {
+      return router.currentRoute.value.meta.title;
+    });
+
+    const hasUnreadMessag = computed(() => {
+      return Math.floor(Math.random() * 10) % 2 === 0;
+    });
 
     return {
-      leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
+      selectedTab,
+      backButtonVisibility,
+      getTitle,
+      hasUnreadMessag,
+
+      onBack() {
+        router.back();
       },
     };
   },
