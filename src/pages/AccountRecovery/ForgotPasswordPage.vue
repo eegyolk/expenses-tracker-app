cc95d899a8d92259e8a30879158735ca555bcd52
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-10-14 16:19:35 +0800

    Created related pages for account recovery

---
+++ src/pages/AccountRecovery/ForgotPasswordPage.vue
@@ -0,0 +1,84 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-card flat class="my-card">
+      <q-card-section class="q-gutter-md">
+        <div class="text-h6 text-center">Forgot Password?</div>
+        <div class="text-caption text-justify">
+          Don't worry! It happens. Please enter the email address associated
+          with your account.
+        </div>
+      </q-card-section>
+
+      <q-card-section>
+        <div class="full-width">
+          <q-input
+            v-model="emailAddress"
+            color="deep-purple-13"
+            placeholder="Your email address"
+            type="email"
+            outlined
+            dense
+          />
+        </div>
+      </q-card-section>
+
+      <q-card-section class="text-center">
+        <q-btn
+          color="deep-purple-13"
+          label="Submit"
+          style="width: 134px"
+          no-caps
+          @click="onSubmit"
+        />
+      </q-card-section>
+
+      <q-separator class="q-my-sm" unset />
+
+      <q-card-section class="full-width text-center">
+        <span class="q-px-xs text-caption">Do you have your password?</span>
+
+        <q-btn
+          label="Login now."
+          class="text-caption text-weight-bold"
+          color="deep-purple-13"
+          padding="none"
+          to="/login"
+          no-caps
+          flat
+          unelavated
+        />
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  name: "ForgotPasswordPage",
+
+  setup() {
+    const emailAddress = ref("");
+
+    const router = useRouter();
+
+    return {
+      emailAddress,
+
+      onSubmit() {
+        router.replace({ name: "verify-reset" });
+      },
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.my-card {
+  width: 100%;
+  max-width: 350px;
+  padding: 25px;
+}
+</style>
---
+++ src/pages/AccountRecovery/ResetPasswordPage.vue
@@ -0,0 +1,108 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-card flat class="my-card">
+      <q-card-section class="q-gutter-md">
+        <div class="text-h6 text-center q-pt-lg">Reset Password</div>
+
+        <div class="q-gutter-md">
+          <div class="full-width">
+            <span>Password</span>
+            <q-input
+              v-model="password"
+              color="deep-purple-13"
+              placeholder="Your password"
+              :type="passwordVisibility ? '' : 'password'"
+              outlined
+              dense
+            >
+              <template v-slot:append>
+                <q-icon
+                  class="cursor-pointer"
+                  :name="passwordVisibility ? 'visibility' : 'visibility_off'"
+                  @click="onTogglePasswordVisibility"
+                />
+              </template>
+            </q-input>
+          </div>
+
+          <div class="full-width">
+            <span>Confirm Password</span>
+            <q-input
+              v-model="confirmPassword"
+              color="deep-purple-13"
+              placeholder="Confirm your password"
+              :type="confirmPasswordVisibility ? '' : 'password'"
+              outlined
+              dense
+            >
+              <template v-slot:append>
+                <q-icon
+                  class="cursor-pointer"
+                  :name="
+                    confirmPasswordVisibility ? 'visibility' : 'visibility_off'
+                  "
+                  @click="onToggleConfirmPasswordVisibility"
+                />
+              </template>
+            </q-input>
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-card-section class="text-center">
+        <q-btn
+          color="deep-purple-13"
+          label="Submit"
+          style="width: 134px"
+          no-caps
+          @click="onSubmit"
+        />
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  name: "ResetPasswordPage",
+
+  setup() {
+    const password = ref("");
+    const confirmPassword = ref("");
+    const passwordVisibility = ref(false);
+    const confirmPasswordVisibility = ref(false);
+
+    const router = useRouter();
+
+    return {
+      password,
+      confirmPassword,
+      passwordVisibility,
+      confirmPasswordVisibility,
+
+      onTogglePasswordVisibility() {
+        passwordVisibility.value = !passwordVisibility.value;
+      },
+
+      onToggleConfirmPasswordVisibility() {
+        confirmPasswordVisibility.value = !confirmPasswordVisibility.value;
+      },
+
+      onSubmit() {
+        router.replace({ name: "login" });
+      },
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.my-card {
+  width: 100%;
+  max-width: 350px;
+  padding: 25px;
+}
+</style>
