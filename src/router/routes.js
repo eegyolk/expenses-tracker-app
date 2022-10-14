c7a0b5ca008acd1572cc5e17fec1a78a6aef049a
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-10-14 18:24:31 +0800

    Added meta in routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,12 +1,16 @@
 const routes = [
   {
     path: "/",
-    component: () => import("layouts/NoLayout.vue"),
+    component: () => import("src/layouts/NonTrackerLayout.vue"),
     children: [
       {
         path: "",
         name: "index",
         component: () => import("pages/IndexPage.vue"),
+        meta: {
+          title: "Welcome",
+          withBackButton: false,
+        },
       },
     ],
   },
@@ -14,51 +18,67 @@ const routes = [
   // BEGIN - Account Creation Pages
   {
     path: "/register",
-    component: () => import("layouts/NoLayout.vue"),
+    component: () => import("src/layouts/NonTrackerLayout.vue"),
     children: [
       {
         path: "",
         name: "register",
         component: () =>
           import("src/pages/AccountCreation/RegistrationPage.vue"),
+        meta: {
+          title: "Registration",
+          withBackButton: true,
+        },
       },
     ],
   },
 
   {
     path: "/verify-account",
-    component: () => import("layouts/NoLayout.vue"),
+    component: () => import("src/layouts/NonTrackerLayout.vue"),
     children: [
       {
         path: "",
         name: "verify-account",
         component: () =>
           import("src/pages/AccountCreation/VerificationPage.vue"),
+        meta: {
+          title: "Account Verification",
+          withBackButton: false,
+        },
       },
     ],
   },
 
   {
     path: "/terms",
-    component: () => import("layouts/NoLayout.vue"),
+    component: () => import("src/layouts/NonTrackerLayout.vue"),
     children: [
       {
         path: "",
         name: "terms",
         component: () => import("src/pages/AccountCreation/TermsPage.vue"),
+        meta: {
+          title: "Terms of Service",
+          withBackButton: true,
+        },
       },
     ],
   },
 
   {
     path: "/privacy-policy",
-    component: () => import("layouts/NoLayout.vue"),
+    component: () => import("src/layouts/NonTrackerLayout.vue"),
     children: [
       {
         path: "",
         name: "privacy-policy",
         component: () =>
           import("src/pages/AccountCreation/PrivacyPolicyPage.vue"),
+        meta: {
+          title: "Privacy Policy",
+          withBackButton: true,
+        },
       },
     ],
   },
@@ -67,12 +87,16 @@ const routes = [
   // BEGIN - Authentication Pages
   {
     path: "/login",
-    component: () => import("layouts/NoLayout.vue"),
+    component: () => import("src/layouts/NonTrackerLayout.vue"),
     children: [
       {
         path: "",
         name: "login",
         component: () => import("src/pages/Authentication/LoginPage.vue"),
+        meta: {
+          title: "Login",
+          withBackButton: true,
+        },
       },
     ],
   },
@@ -81,39 +105,51 @@ const routes = [
   // BEGIN - Account Recovery Pages
   {
     path: "/forgot-password",
-    component: () => import("layouts/NoLayout.vue"),
+    component: () => import("src/layouts/NonTrackerLayout.vue"),
     children: [
       {
         path: "",
         name: "forgot-password",
         component: () =>
           import("src/pages/AccountRecovery/ForgotPasswordPage.vue"),
+        meta: {
+          title: "Forgot Password",
+          withBackButton: true,
+        },
       },
     ],
   },
 
   {
     path: "/verify-reset",
-    component: () => import("layouts/NoLayout.vue"),
+    component: () => import("src/layouts/NonTrackerLayout.vue"),
     children: [
       {
         path: "",
         name: "verify-reset",
         component: () =>
           import("src/pages/AccountRecovery/VerificationPage.vue"),
+        meta: {
+          title: "Reset Verification",
+          withBackButton: false,
+        },
       },
     ],
   },
 
   {
     path: "/reset-password",
-    component: () => import("layouts/NoLayout.vue"),
+    component: () => import("src/layouts/NonTrackerLayout.vue"),
     children: [
       {
         path: "",
         name: "reset-password",
         component: () =>
           import("src/pages/AccountRecovery/ResetPasswordPage.vue"),
+        meta: {
+          title: "Reset Password",
+          withBackButton: false,
+        },
       },
     ],
   },
@@ -122,12 +158,16 @@ const routes = [
   // BEGIN - Tracker Pages
   {
     path: "/tracker/home",
-    component: () => import("layouts/MainLayout.vue"),
+    component: () => import("src/layouts/TrackerLayout.vue"),
     children: [
       {
         path: "",
         name: "tracker-home",
         component: () => import("src/pages/Tracker/HomePage.vue"),
+        meta: {
+          title: "Home",
+          withBackButton: false,
+        },
       },
     ],
   },
