b4b5ba1e7d1f077d245b6c35947dffe61ea502e4
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-10-14 23:11:57 +0800

    Created more store

---
+++ src/stores/account-recovery-store.js
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia";
+
+export const useAccountRecoveryStore = defineStore("accountRecovery", {
+  state: () => ({
+    counter: 0,
+  }),
+  getters: {
+    doubleCount: (state) => state.counter * 2,
+  },
+  actions: {
+    increment() {
+      this.counter++;
+    },
+  },
+});
