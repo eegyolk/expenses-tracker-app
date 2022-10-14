6dd8c419753befe1a6b715d66c39bc8a623db163
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-10-14 18:28:02 +0800

    Display title and back button

---
+++ src/layouts/NonTrackerLayout.vue
@@ -0,0 +1,59 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-deep-purple-13" elevated>
+      <q-toolbar>
+        <div class="col-1 col-sm-1">
+          <q-btn
+            v-if="backButtonVisibility"
+            icon="west"
+            aria-label="Back"
+            round
+            dense
+            flat
+            @click="onBack"
+          />
+        </div>
+
+        <div class="col col-sm text-center">
+          <span class="text-h6">{{ getTitle }}</span>
+        </div>
+
+        <div class="col-1 col-sm-1"></div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, computed } from "vue";
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  name: "NoLayout",
+
+  setup() {
+    const router = useRouter();
+
+    const backButtonVisibility = computed(() => {
+      return router.currentRoute.value.meta.withBackButton;
+    });
+
+    const getTitle = computed(() => {
+      return router.currentRoute.value.meta.title;
+    });
+
+    return {
+      backButtonVisibility,
+      getTitle,
+
+      onBack() {
+        router.back();
+      },
+    };
+  },
+});
+</script>
