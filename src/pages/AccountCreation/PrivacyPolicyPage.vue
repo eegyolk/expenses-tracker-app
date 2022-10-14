a1bb02d4f3d0775b4e706abe1a7f2d967b84e73f
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-10-14 21:23:07 +0800

    Updated terms and privacy policy

--- src/pages/AccountCreation/PrivacyPolicyPage.vue
+++ src/pages/AccountCreation/PrivacyPolicyPage.vue
@@ -1,16 +1,15 @@
 <template>
   <q-page class="flex flex-center">
     <q-card flat>
-      <q-card-section class="bg-deep-purple-13">
-        <div class="text-h6 text-center q-pt-lg text-white">Privacy Policy</div>
-      </q-card-section>
-
       <q-card-section class="full-width text-center">
-        <p class="q-px-xs text-caption" v-for="n in 15" :key="n">
+        <p class="q-px-xs text-justify text-caption" v-for="n in 10" :key="n">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
           repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis
           perferendis totam, ea at omnis vel numquam exercitationem aut, natus
-          minima, porro labore.
+          minima, porro labore. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet
+          porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam
+          exercitationem aut, natus minima, porro labore.
         </p>
       </q-card-section>
     </q-card>
