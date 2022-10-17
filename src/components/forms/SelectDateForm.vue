d5ebbe24937afb2612a45557108839934224e6c2
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-10-17 14:40:43 +0800

    Created new component SelectDateForm

---
+++ src/components/forms/SelectDateForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-date
+    v-model="date"
+    color="deep-purpe-13"
+    today-btn
+    @update:model-value="onSelectDate"
+  />
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import { date } from "quasar";
+
+export default defineComponent({
+  name: "SelectDateForm",
+
+  props: {
+    selectedDate: {
+      type: String,
+      default: "",
+    },
+  },
+
+  setup(props, { emit }) {
+    const date = ref(props.selectedDate);
+
+    return {
+      date,
+      onSelectDate(value) {
+        emit("input", value);
+      },
+    };
+  },
+});
+</script>
