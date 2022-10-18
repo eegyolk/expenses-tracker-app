e3c5108971723feac50930532b40735a2a434aa2
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-10-18 14:32:27 +0800

    Implemented ListSection component in Income index page

--- src/pages/Tracker/Income/IndexPage.vue
+++ src/pages/Tracker/Income/IndexPage.vue
@@ -67,41 +67,7 @@
           </div>
 
           <div class="full-width">
-            <q-scroll-area
-              :thumb-style="thumbStyle"
-              :bar-style="barStyle"
-              style="height: 450px"
-              id="scroll-area-with-virtual-scroll-1"
-            >
-              <q-virtual-scroll
-                scroll-target="#scroll-area-with-virtual-scroll-1 > .scroll"
-                :items="heavyList"
-                :virtual-scroll-item-size="32"
-                separator
-                v-slot="{ item, index }"
-              >
-                <q-item :key="index">
-                  <q-item-section top avatar>
-                    <q-avatar
-                      color="deep-purple-13"
-                      text-color="white"
-                      icon="money"
-                    />
-                  </q-item-section>
-
-                  <q-item-section>
-                    <q-item-label>Salary {{ item.label }}</q-item-label>
-                    <q-item-label caption lines="2">
-                      Lorem ipsum dolor sit amet, consectetur adipiscit elit.
-                    </q-item-label>
-                  </q-item-section>
-
-                  <q-item-section side>
-                    <span>3,000.00</span>
-                  </q-item-section>
-                </q-item>
-              </q-virtual-scroll>
-            </q-scroll-area>
+            <list-section />
           </div>
         </q-card-section>
       </q-card>
@@ -112,21 +78,14 @@
 <script>
 import { defineComponent, ref } from "vue";
 import FilterBySection from "components/sections/FilterBySection.vue";
-
-const maxSize = 100;
-const heavyList = [];
-
-for (let i = 0; i < maxSize; i++) {
-  heavyList.push({
-    label: "Option " + (i + 1),
-  });
-}
+import ListSection from "components/sections/ListSection.vue";
 
 export default defineComponent({
   name: "IndexPage",
 
   components: {
     FilterBySection,
+    ListSection,
   },
 
   setup() {
@@ -138,26 +97,6 @@ export default defineComponent({
       dateFrom,
       dateTo,
       sort,
-
-      heavyList,
-      thumbStyle: {
-        right: "5px",
-        borderRadius: "8px",
-        backgroundColor: "#6520FF",
-        width: "8px",
-        opacity: 0.75,
-      },
-      barStyle: {
-        right: "2px",
-        borderRadius: "14px",
-        backgroundColor: "#6520FF",
-        width: "14px",
-        opacity: 0.2,
-        marginTop: "-3px",
-        marginBottom: "-3px",
-        paddingTop: "3px",
-        paddingBottom: "3px",
-      },
 
       onSelectFilterDateRange({ from, to }) {
         dateFrom.value = from;
