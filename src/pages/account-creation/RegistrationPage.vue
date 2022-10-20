d7c42c33022cbca2381eea94692fb335cfa07c85
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-10-21 07:49:06 +0800

    Updated RegistrationPage UI

--- src/pages/account-creation/RegistrationPage.vue
+++ src/pages/account-creation/RegistrationPage.vue
@@ -4,18 +4,17 @@
       <img src="~assets/register-img.svg" style="width: 200px; height: 200px" />
 
       <div class="q-pt-md text-center" :style="{ width: `${windowWidth}px` }">
-        <span class="q-mx-md text-h6 text-weight-medium text-grey-9">
+        <span class="text-h6 text-weight-medium text-grey-9">
           Create your account
         </span>
       </div>
 
       <div class="q-pt-md text-left" :style="{ width: `${windowWidth}px` }">
-        <span class="q-mx-md text-body2 text-weight-medium text-grey-9">
+        <span class="text-body2 text-weight-medium text-grey-9">
           Full Name
         </span>
         <q-input
           v-model="fullName"
-          class="q-mx-md"
           color="deep-purple-13"
           placeholder="Your full name"
           outlined
@@ -24,12 +23,11 @@
       </div>
 
       <div class="q-pt-md text-left" :style="{ width: `${windowWidth}px` }">
-        <span class="q-mx-md text-body2 text-weight-medium text-grey-9">
+        <span class="text-body2 text-weight-medium text-grey-9">
           Email Address
         </span>
         <q-input
           v-model="emailAddress"
-          class="q-mx-md"
           color="deep-purple-13"
           placeholder="Your email address"
           type="email"
@@ -39,12 +37,11 @@
       </div>
 
       <div class="q-pt-md text-left" :style="{ width: `${windowWidth}px` }">
-        <span class="q-mx-md text-body2 text-weight-medium text-grey-9">
+        <span class="text-body2 text-weight-medium text-grey-9">
           Password
         </span>
         <q-input
           v-model="password"
-          class="q-mx-md"
           color="deep-purple-13"
           placeholder="Your password"
           :type="passwordVisibility ? '' : 'password'"
@@ -61,10 +58,7 @@
         </q-input>
       </div>
 
-      <div
-        class="q-pt-md q-pt-lg text-center"
-        :style="{ width: `${windowWidth}px` }"
-      >
+      <div class="q-pt-lg text-center" :style="{ width: `${windowWidth}px` }">
         <q-toggle
           v-model="acceptTOSAndPP"
           color="deep-purple-13"
@@ -98,33 +92,37 @@
           unelavated
         />
 
-        <span class="text-caption">.</span>
+        <span class="text-caption text-grey-9">.</span>
       </div>
     </div>
 
-    <div class="row wrap justify-center" :style="{ width: `${windowWidth}px` }">
-      <q-btn
-        class="full-width q-mb-lg"
-        color="deep-purple-13"
-        label="Register"
-        no-caps
-        @click="onRegister"
-      />
+    <div class="row wrap justify-center full-width">
+      <div class="text-center" :style="{ width: `${windowWidth}px` }">
+        <q-btn
+          class="full-width q-mb-lg"
+          color="deep-purple-13"
+          label="Register"
+          no-caps
+          @click="onRegister"
+        />
+      </div>
 
-      <span class="q-px-xs text-caption text-grey-9"
-        >Already have an account?</span
-      >
+      <div class="text-center" :style="{ width: `${windowWidth}px` }">
+        <span class="q-px-xs text-caption text-grey-9"
+          >Already have an account?</span
+        >
 
-      <q-btn
-        label="Login now."
-        class="text-caption text-weight-bold"
-        color="deep-purple-13"
-        padding="none"
-        to="/login"
-        no-caps
-        flat
-        unelavated
-      />
+        <q-btn
+          label="Login now."
+          class="text-caption text-weight-bold"
+          color="deep-purple-13"
+          padding="none"
+          to="/login"
+          no-caps
+          flat
+          unelavated
+        />
+      </div>
     </div>
   </q-page>
 </template>
