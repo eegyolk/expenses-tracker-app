98bee554f7e6a1d7e7f20d59d0052adf8e26e2bd
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-10-20 21:46:27 +0800

    Created usePage composable

---
+++ src/composables/page.js
@@ -0,0 +1,31 @@
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import { useQuasar, QSpinnerIos } from "quasar";
+
+export function usePage() {
+  const visiblePage = ref(false);
+
+  let timer;
+  const $q = useQuasar();
+
+  onMounted(() => {
+    $q.loading.show({
+      spinner: QSpinnerIos,
+      spinnerColor: "deep-purple-13",
+      spinnerSize: "md",
+      backgroundColor: "white",
+    });
+
+    timer = setTimeout(() => {
+      $q.loading.hide();
+      visiblePage.value = true;
+    }, 1000);
+  });
+
+  onBeforeUnmount(() => {
+    if (timer !== void 0) {
+      clearTimeout(timer);
+    }
+  });
+
+  return { visiblePage };
+}
